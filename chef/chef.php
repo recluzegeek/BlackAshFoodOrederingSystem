516b9126019edd44b82dc2868466f23cabafdef0
Salman1057 <sabsalman14@gmail.com>
2023-04-03 16:30:42 +0000

    Chef View Completed

--- chef/chef.php
+++ chef/chef.php
@@ -1,204 +1,192 @@
 <?php
 session_start();
-//if (!isset($_SESSION['chef_username'])){
-//  $message = 'You are not logged in. You will be redirected to the login page.';
-//  echo "<script type='text/javascript'>alert('$message');</script>"; // Show an alert message
-//echo '<script>setTimeout(function(){ window.location.href = "./login.html"; }, 100);</script>'; //redirecting user to login.html
-//exit();}
+include '../php/connection.php';
+if (!isset($_SESSION['chef_username'])) {
+  $message = 'You are not logged in. You will be redirected to the login page.';
+  echo "<script type='text/javascript'>alert('$message');</script>"; // Show an alert message
+  echo '<script>setTimeout(function(){ window.location.href = "./login.html"; }, 100);</script>'; //redirecting user to login.html
+  exit();
+}
+$stmt = $conn->prepare("SELECT name FROM chef WHERE email = ?");
+$stmt->bind_param("s", $_SESSION['chef_username']);
+$stmt->execute();
+$stmt->bind_result($chefName);
+$stmt->fetch();
+$stmt->close();
+$conn->close();
 ?>
 
-<html lang="en">
+<!DOCTYPE html>
 <head>
   <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../css/chef.css">
+  <meta http-equiv="X-UA-Compatible"
+        content="IE=edge">
+  <meta name="viewport"
+        content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet"
+        href="../css/tables.css">
   <title>CHEF &#8211; Black Ash Restaurant</title>
 </head>
 <body>
 
-<header class="chef-header">
-  <h3 class="header-title">Welcome to Black Ash Restaurant Chef's Corner</h3>
-  <img id="logo" src="../img/cropped-blackashlogo@4x.webp"
-       alt="nice logo">
-  <div class="account-icon">
-    <div class="account-options">
-      <div class="option">Change Password</div>
-      <div class="option">Profile</div>
-      <a href="../php/logout.php?logout=chef">Logout</a>
+<div class="nav-bar">
+  <div class="logo-wrapper">
+    <img style="height: 64px; width: 64px"
+         src="../img/cropped-blackashlogo@4x.webp"
+         alt="logo">
+    <h1 style="text-transform: uppercase;color: white;font-weight: 900">Black Ash</h1>
+  </div>
+  <ul>
+    <li><a href="./customer.php">Homepage</a></li>
+    <li><a href="#"
+           onclick="toggleContainer('chef-container')">Orders</a></li>
+    <li><a href="#">Account</a></li>
+    <li><a href="#"
+           onclick="location.reload()">Help</a></li>
+    <li><a href="../php/logout.php?logout=chef">Logout</a></li>
+  </ul>
+</div>
+
+<main>
+  <div id="main-container"
+       class="container">
+    <p style="text-align: center; font-size: 1.7rem; font-weight: 900">Welcome back, Chef <?php echo $chefName ?>
+      !</p>
+    <p class="welcome-screen-tagline">"Start managing orders by selecting 'Orders' from the top right navigation
+      menu."</p>
+    <!--            <br>-->
+    <!--            <br>-->
+    <p class="welcome-screen-tagline">
+      "Choose the 'Pending' tab to see orders that require your attention."
+    </p>
+    <!--            <br>-->
+    <!--            <br>-->
+    <p class="welcome-screen-tagline">"Your attention to detail and commitment to quality service will make all the
+      difference. Let's get
+      started!"
+    </p>
+  </div>
+
+  <?php
+  include '../php/connection.php';
+  $sql = "SELECT o.num, a.item_id, q.quantity, i.name
+    FROM food_order o
+    JOIN adds a ON o.num = a.order_num
+    JOIN order_quantity q ON a.item_id = q.item_id AND o.num = q.order_num
+    JOIN item i on a.item_id = i.id
+    WHERE o.status = 'processing'
+    ORDER BY o.date_time DESC";
+  $result = $conn->query($sql);
+  ?>
+
+  <div id="chef-container"
+       class="container"
+       style="display: none">
+    <div>
+      <input type="checkbox"
+             style="opacity: 0"
+             id="select-all-orders"
+             onclick="selectAllOrders()">
+      <label class="anchor-styling"
+             for="select-all-orders">Select All Orders</label>
+    </div>
+    <div id="container"
+         style="display: flex;flex-direction: inherit;">
+      <div class="table-container"
+           style="width: 90%">
+        <ul class="responsive-table"
+            style="padding: 0">
+          <li class="table-header">
+            <div class="col col-1"
+                 style="flex-basis: 20%">Serial
+              #<span class="sort-symbol asc">&#9660;</span><span class="sort-symbol desc">&#9650;</span>
+            </div>
+            <div class="col col-2"
+                 style="flex-basis: 25%">Order
+              #<span class="sort-symbol asc">&#9660;</span><span class="sort-symbol desc">&#9650;</span>
+            </div>
+            <div class="col col-3"
+                 style="display: none"></div>
+            <div class="col col-4"
+                 style="display: none"></div>
+            <div class="col col-5"
+                 style="display: none"></div>
+            <div class="col col-3"
+                 style="flex-basis: 50%">Order Details
+            </div>
+          </li>
+          <?php
+          $prev_order_num = null;
+          $serial_num = 1;
+          if ($result->num_rows > 0) {
+          while ($row = $result->fetch_assoc()) {
+          $order_num = $row['num'];
+          $item_name = $row['name'];
+          $quantity = $row['quantity'];
+
+          if ($order_num != $prev_order_num) {
+          // Start a new order row
+          ?>
+          <li class="table-row order-row">
+            <div><input type="checkbox"
+                        name="order[]"
+                        value="<?php echo $row['num'] ?>"></div>
+            <div class="col col-1"
+                 style="flex-basis: 20%"
+                 data-label="Serial Num"><?php echo "#" . $serial_num++; ?></div>
+            <div class="col col-2"
+                 style="flex-basis: 25%"
+                 data-label="Order Num"><?php echo "#" . $order_num ?></div>
+            <div class="col col-3"
+                 style="display: none"></div>
+            <div class="col col-4"
+                 style="display: none"></div>
+            <div class="col col-5"
+                 style="display: none"></div>
+            <div class="col col-3"
+                 style="flex-basis: 50%"
+                 data-label="Order Details">
+              <div class="order-item-container">
+                <div class="order-item-name"
+                     style="color: black;font-weight: 900;font-size: 1.15rem;">Item Name x Quantity
+                </div>
+                <div class="order-item-name"><?php echo $item_name . ' x ' . $quantity ?></div>
+              </div>
+              <?php
+              } else {
+                // Continue the previous order row
+                ?>
+                <div class="order-item-container">
+                  <div class="order-item-name"><?php echo $item_name . ' x ' . $quantity ?></div>
+                </div>
+                <?php
+              }
+              $prev_order_num = $order_num;
+              }
+              } else {
+                echo "<p>No orders found.</p>";
+              }
+              ?>
+            </div>
+          </li>
+        </ul>
+        <button class="anchor-styling"
+                id="send-selected-orders-btn"
+                onclick="sendSelectedOrders('processing')">Ready For Takeaway
+        </button>
+      </div>
     </div>
   </div>
-</header>
+</main>
 
 
-<div class="container">
-  <button class="collapsible" id="pending-orders-btn">Pending Orders</button>
-  <div class="content" id="pending-orders-row">
-    <table id="table1">
-      <tr>
-        <th>Order ID</th>
-        <th>Customization</th>
-        <th>Items</th>
-        <th>Action</th>
-      </tr>
-      <tr>
-        <td>1</td>
-        <td>No extra spice</td>
-        <td>Pizza, burger, fries</td>
-        <td><input type="checkbox" name="check-tab1"></td>
-
-      </tr>
-      <tr>
-        <td>2</td>
-        <td>Extra cheese</td>
-        <td>Pizza, fries</td>
-        <td><input type="checkbox" name="check-tab1"></td>
-      </tr>
-      <tr>
-        <td>3</td>
-        <td>No onions</td>
-        <td>Burger, fries</td>
-        <td><input type="checkbox" name="check-tab1"></td>
-      </tr>
-      <tr>
-        <td>4</td>
-        <td>No tomatoes</td>
-        <td>Pizza</td>
-        <td><input type="checkbox" name="check-tab1"></td>
-      </tr>
-      <tr>
-        <td>5</td>
-        <td>Extra spicy</td>
-        <td>Burger</td>
-        <td><input type="checkbox" name="check-tab1"></td>
-      </tr>
-    </table>
-  </div>
-
-  <div>
-    <button onclick="tab1_To_tab2();" class="collapsible">
-      <span>Ready for Takeaway</span>
-      <svg viewBox="0 0 13 10" height="10px" width="15px">
-        <path d="M1,5 L11,5"></path>
-        <polyline points="8 1 12 5 8 9"></polyline>
-      </svg>
-    </button>
-  </div>
-
-  <button class="collapsible" id="completed-orders-btn">Completed Orders</button>
-  <div class="content" id="completed-orders-row">
-    <table id="table2">
-      <tr>
-        <th>Order ID</th>
-        <th>Customization</th>
-        <th>Items</th>
-        <th>Completed</th>
-      </tr>
-    </table>
-  </div>
-</div>
+<footer>
+  <p>Copyright © 2023 Black Ash Food Ordering System. All rights reserved.</p>
+</footer>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="../js/data_sort.js"></script>
 
 
 </body>
-
-<script>
-  document.getElementById('logout').onclick = function () {
-    location.href = './login.html';
-  };
-
-
-  // Get the buttons that toggle the collapsible rows
-  const pendingOrdersBtn = document.getElementById("pending-orders-btn");
-  const completedOrdersBtn = document.getElementById("completed-orders-btn");
-
-  // Get the collapsible rows
-  const pendingOrdersRow = document.getElementById("pending-orders-row");
-  const completedOrdersRow = document.getElementById("completed-orders-row");
-
-  // Function to toggle the display of a collapsible row
-  function toggleCollapse(element) {
-    if (element.style.display === "none") {
-      element.style.display = "block";
-    } else {
-      element.style.display = "none";
-    }
-  }
-
-  // Add click event listeners to the toggle buttons
-  pendingOrdersBtn.addEventListener("click", function () {
-    toggleCollapse(pendingOrdersRow);
-  });
-
-  completedOrdersBtn.addEventListener("click", function () {
-    toggleCollapse(completedOrdersRow);
-  });
-
-  // Hide the collapsible rows by default
-  pendingOrdersRow.style.display = "";
-  completedOrdersRow.style.display = "none";
-
-
-  // Function to Switch Rows from One Table to another
-
-  function tab1_To_tab2() {
-
-    const table1 = document.getElementById("table1"),
-
-      table2 = document.getElementById("table2");
-
-    const checkboxes = document.getElementsByName("check-tab1");
-
-    console.log(" Val1 = " + checkboxes.length);
-
-    for (let i = 0; checkboxes.length; i++) {
-
-      if (checkboxes[i].checked) {
-
-
-        // Creates new rows and cells
-
-        const newRow = table2.insertRow(table2.length),
-
-          cell1 = newRow.insertCell(0),
-
-          cell2 = newRow.insertCell(1),
-
-          cell3 = newRow.insertCell(2),
-
-          cell4 = newRow.insertCell(3);
-
-
-        // Adds values to the cells
-
-        cell1.innerHTML = table1.rows[i + 1].cells[0].innerHTML;
-
-        cell2.innerHTML = table1.rows[i + 1].cells[1].innerHTML;
-
-        cell3.innerHTML = table1.rows[i + 1].cells[2].innerHTML;
-
-        cell4.innerHTML = "<input type='checkbox' name='check-tab2' checked disabled>";
-
-
-        // Remove the transferred rows from the first table[table1]
-
-        const index = table1.rows[i + 1].rowIndex;
-
-        table1.deleteRow(index);
-
-        // We have deleted some rows, so the checkboxes.length have changed.
-
-        // So, we have to decrement the value of i.
-
-        i--;
-
-        console.log(checkboxes.length);
-
-      }
-
-    }
-
-  }
-
-</script>
 
 </html>
